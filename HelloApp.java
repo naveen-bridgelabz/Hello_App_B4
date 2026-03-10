4f73c2642c0c384b1b0a85aade85eb462c14f4b2
naveen-bridgelabz <naveen.kumar@bridgelabz.com>
2026-03-10 13:44:43 +0530

    Dev branch created

--- HelloApp.java
+++ HelloApp.java
@@ -1,9 +1,11 @@
+/*
+@author Naveen
+@version 1.0
+*/
+
 class HelloApp{
 	public static void main(String[] args){
-		//variables & Datatypes
-		int num = 100;
 
-		System.out.println(num);
-		System.out.println("Hello All...");
+		System.out.println("this is Dev Branch");
 	}
 }
